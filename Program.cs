1cf1e2d05d3a7703e4ef5839b2fc32d9e89d40f1
yangbocheng <419445583@qq.com>
2023-10-26 10:22:21 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new testAuto设置123().test();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ testAuto设置123.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class testAuto设置123
+    {
+        public int Add(int x, int y)
+        {
+            var a = x + y;
+            return a;
+        }
+
+        public void test()
+        {
+            var filepath = "result.docx";
+
+            var a = Add(5, 7);
+
+
+        }
+    }
+}
